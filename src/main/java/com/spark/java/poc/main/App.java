137fa6c1869d142a41277aea6a6159978067cd1e
Ashok <k.ashokachary@gmail.com>
2020-12-27 10:41:37 +0530

    Initial check-in of Project setup

---
+++ src/main/java/com/spark/java/poc/main/App.java
@@ -0,0 +1,13 @@
+package com.spark.java.poc.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
